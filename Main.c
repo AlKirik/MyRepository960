c42bd0d3e0ffce0e7d3e64c315555878f75123f9
Кирилл <93206821+AlKirik@users.noreply.github.com>
2021-12-24 18:57:06 +0300

    Lab5 Prog3

--- Main.c
+++ Main.c
@@ -4,12 +4,16 @@
 int main() {
 	setlocale(LC_ALL, "Rus");
 	int a, b, c, ac, bc, sum;
-	scanf_s("%d",&a);
-	scanf_s("%d",&b);
-	scanf_s("%d",&c);
-	ac = abs(c - a);
-	bc = abs(b - c);
-	sum = ac + bc;
-	printf("AC = %d\nBC = %d\nСУММА = %d\n",ac,bc,sum);
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+	if (c > b || c < a) { printf("Точка С не находится между точками А и В, условие не выполнено\n"); }
+	else {
+		;
+		ac = abs(c - a);
+		bc = abs(b - c);
+		sum = ac * bc;
+		printf("AC = %d\nBC = %d\nПроизведение длин отрезков = %d\n", ac, bc, sum);
+	}
 	return 0;
 }
